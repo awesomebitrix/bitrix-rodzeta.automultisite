4e55b5690c1f4895d919696ccad26f602c28905e
Rick Deckard <xxxblasteroid@gmail.com>
2016-11-29 15:38:46 +0300

    fix

--- install/data/.auto_prepend_file.php
+++ install/data/.auto_prepend_file.php
@@ -15,13 +15,12 @@ if (parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) == "/") {
 		// dest url
 		//	renessans-park.villa-mia.ru/ -> villa-mia.ru/poselki/renessans-park/
 		$_SERVER["REDIRECT_URL"] = $_SERVER["REQUEST_URI"] = "/poselki/" . $tmpSectionCode . "/";
+		unset($tmpSectionCode);
 
 		// bitrix entry point
 		$_SERVER["PHP_SELF"] = $_SERVER["SCRIPT_NAME"] = "/bitrix/urlrewrite.php";
 		$_SERVER["SCRIPT_FILENAME"] = $_SERVER["DOCUMENT_ROOT"] . "/bitrix/urlrewrite.php";
 		$_SERVER["REDIRECT_STATUS"] = "200";
-
-		unset($tmpSectionCode);
 		require $_SERVER["SCRIPT_FILENAME"];
 		die;
 	}
